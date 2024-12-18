cb0ed5a8a915728bb11a3af461b2b81b9a8a5462
GbemigadEngineer <142888513+GbemigadEngineer@users.noreply.github.com>
2024-12-19 00:16:52 +0100

    Add files via upload

---
+++ What should the calculator app look like.md
@@ -0,0 +1,11 @@
+What should the calculator app look like?
+
+Its single page so need for a nav bar too cause you are not navigating anywhere.
+
+first you input your details
+User Details are: Name,Age, School, level
+GPA data details: what semester, number of courses that semsester: this would then create a courswe table with the exact number of roles you put for number of courses.
+The GPA Table The first column would be courses, second would be total score over 100, 3rd would be an automatic grade score set based on the total score. By default the grade score would be set based on my school(Bacock University standards), but you can customize it to your schools own if its not the same grading system your school is using. E.g in BU 70 is Grade A in your school 70 could be B so to change that just click on the customize button on the head of that column and it would show a like form or whatever where you set the intervals for the score for your preference. Then the next colum would be credit Unit, the the next would be the GP for that course.
+AT the bottom It would calculate CGPA and GPA based on the data in the table and siplay your GP is: and your CGPA is:
+Then there would be a add new semester button that you can add a completely new GPA data details but the CGPA would be the same but the GP would be reset to zero for that new semester. Then your CGPA would be the addition of the last GP plus the new GP divided by the total number of Gps.
+-
---
+++ script.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const userLevel = document.getElementById("level");
+const submitButton = document.querySelector("#submit");
+const invalidMessage = document.querySelector(".hidden");
+
+const addGradeButton = document.getElementById("add-grade");
+const gradesContainer = document.getElementById("grades-container");
+
+const levelList = [
+  100,
+  200,
+  300,
+  400,
+  500,
+  600,
+  700,
+  "Phd",
+  "Masters",
+  "Diploma",
+];
+
+// Action once the submit button is clicked
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  // Validate  Form
+  if (levelList.includes(Number(userLevel.value))) {
+    table.classList.remove("hidden");
+    generateTableRows(8);
+  } else {
+    invalidMessage.classList.remove("hidden");
+  }
+});
+
+// grading system
+
+// default grading system
+const defaultGrading = function (score) {
+  if (score >= 0 && score <= 39) {
+    grade = "F";
+  }
+  if (score >= 40 && score <= 44) {
+    grade = "E";
+  }
+  if (score >= 45 && score <= 49) {
+    grade = "D";
+  }
+  if (score >= 50 && score <= 59) {
+    grade = "C";
+  }
+  if (score >= 60 && score <= 74) {
+    grade = "B";
+  }
+  if (score >= 75 && score <= 100) {
+    grade = "A";
+  }
+};
+const defaultCalcGP = function () {
+  if (grade === "F") gp = 0;
+  if (grade === "E") gp = 1;
+  if (grade === "D") gp = 2;
+  if (grade === "C") gp = 3;
+  if (grade === "B") gp = 4;
+  if (grade === "A") gp = 5;
+};
+
